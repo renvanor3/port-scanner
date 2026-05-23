ca648041f2ed488ae79b56033bc59694228de709
ali sait <ali-sait.bardak@etu.univ-amu.fr>
2026-05-23 11:27:52 +0200

    scanner multi-ports sans parallélisation

--- scanner.py
+++ scanner.py
@@ -3,11 +3,18 @@ import socket
 target = "127.0.0.1" #ma machine
 port = 22
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.settimeout(1)
-result = s.connect_ex((target, port))
+def port_scan(target, port, timeout=1):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(timeout)
+    result = s.connect_ex((target, port))
+    s.close()
+    return result==0
 
-if result == 0:
-    print("Port " + str(port) + " is open")
-else:
-    print("Port " + str(port) + " is not open")
+
+
+target = "127.0.0.1" #ma machine
+for port in range(1, 1025):
+    if port_scan(target, port):
+        print("Port " + str(port) + " is open")
+
+print("Scan completed")
